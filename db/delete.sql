84b2dec45e0d065b4cac39249720eb65ac665884
Lucian <lucian.nguyen0902@gmail.com>
2023-11-30 23:06:34 +0700

    edit delete

--- db/delete.sql
+++ db/delete.sql
@@ -1,20 +1,20 @@
--- thủ tục delete sẽ kiểm tra các trang thai cua nhung khach hang neu trang thai la 0 (tuc duoc phep xoa) thi xoa no
+-- De giam thieu du lieu can luu thi chung ta nen xoa nhung tai khoan khong uy tin, thủ tục delete sẽ kiểm tra các trang thai cua nhung khach hang status la true (tuc co the bi vo hieu hoa) neu ho chua thuc hien giao dich nao thi xoa
 USE bookstore;
 DELIMITER //
 CREATE PROCEDURE deleteCustomers()
 BEGIN
     -- neu da xuat hien bang tam thi xoa
     DROP TEMPORARY TABLE IF EXISTS temp_table;
-    -- tao bang tam chua id cua nhung tai khoan khach hang dang khong hoat dong
+    -- tao bang tam chua id cua nhung tai khoan khach hang co status = true va chua thuc hien giao dich nao
     CREATE TEMPORARY TABLE temp_table (idToDelete VARCHAR(10));
     INSERT INTO temp_table
-    SELECT id FROM customer WHERE status != true;
+    SELECT id FROM customer WHERE status = true AND id NOT IN (SELECT customer FROM customerOrder WHERE status = 1);
     IF (SELECT COUNT(*) FROM temp_table) > 0 THEN
-        SELECT 'Da xoa nhung tai khoan khach hang khong hoat dong';
+        SELECT 'Da xoa nhung tai khoan khach hang khong thuc hien giao dich nao';
     ELSE 
-        SELECT 'Moi tai khoan khach hang deu dang hoat dong';
+        SELECT 'Moi tai khoan deu hoat dong';
 	END IF;
-    -- xoa nhung khach hang co tai khoan khong hoat dong
+    -- xoa nhung khach hang co tai khoan chua thuc hien giao dich nao
     DELETE FROM customer WHERE id IN (SELECT idToDelete FROM temp_table);
 END;
 //
